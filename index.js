08fbbc69a34915f07460c3a9c6dd21ecef479c35
Jack Armstrong <jackapollo10@gmail.com>
2024-02-22 12:43:40 +1100

    added team and console.log()

---
+++ index.js
@@ -0,0 +1,60 @@
+const teams = {
+    carlton: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    collingwood: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    melbourne: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    westCoast: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    fremantle: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    GWS: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    goldCoast: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    northMelbourne: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    sydney: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    richmond: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    geelong: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    stKilda: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    essondon: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    brisbane: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    portAdelaide: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    adelaide: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    westernBulldogs: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+    hawthorn: {
+        players: [`Patrick Cripps`, 'Sam Walsh', 'Sam Docherty']
+    },
+}
+
+const astroSign = []
+
+console.log(team[1])
